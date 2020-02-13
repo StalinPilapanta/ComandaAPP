a6bd5272454abe82c02cbe71580a8bc90a163b66
Stalin Pilapanta <pilapantastalin@gmail.com>
2020-02-13 11:01:51 -0500

    crud para pedidos

--- app/src/main/java/com/home/comandaapp/ingresarPedidos.java
+++ app/src/main/java/com/home/comandaapp/ingresarPedidos.java
@@ -1,20 +1,30 @@
 package com.home.comandaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.home.comandaapp.model.Pedidos;
 import com.home.comandaapp.model.Personas;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class ingresarPedidos extends AppCompatActivity {
@@ -23,6 +33,13 @@ public class ingresarPedidos extends AppCompatActivity {
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+
+    private List<Pedidos> listPedidos= new ArrayList<Pedidos>();
+    ArrayAdapter<Pedidos> arrayAdapterPedidos;
+    ListView lvPedidos2;
+
+
+    Pedidos PedidosSelected;
 
 
     @Override
@@ -38,6 +55,26 @@ public class ingresarPedidos extends AppCompatActivity {
         extrasP = findViewById(R.id.edtExtrasP);
 
         inicializarFirebase();
+
+        lvPedidos2 = findViewById(R.id.lvPedidos2);
+        inicializarFirebase();
+        listarPedidos();
+
+        lvPedidos2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                PedidosSelected = (Pedidos) parent.getItemAtPosition(position);
+
+                ///Datos de las personas
+                numeroPedido.setText(PedidosSelected.getNumerPedido());
+                clienteP.setText(PedidosSelected.getNombre());
+                cedulaP.setText(PedidosSelected.getCedula());
+                menuP.setText(PedidosSelected.getMenu());
+                mesaP.setText(PedidosSelected.getMesa());
+                extrasP.setText(PedidosSelected.getExtras());
+                Toast.makeText(getApplicationContext(), "Seleccionado",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -52,10 +89,10 @@ public class ingresarPedidos extends AppCompatActivity {
             guardarDatos();
             Toast.makeText( ingresarPedidos.this, "Agregado",Toast.LENGTH_SHORT).show();
         }else if(id == R.id.icon_save){
-            //actualizarDatos();
+            actualizarPedidos();
             Toast.makeText( ingresarPedidos.this, "Actualizar",Toast.LENGTH_SHORT).show();
         }else if(id == R.id.icon_delete){
-            //eliminarDatos();
+            eliminarPedidos();
             Toast.makeText( ingresarPedidos.this, "Eliminar",Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
@@ -94,6 +131,25 @@ public class ingresarPedidos extends AppCompatActivity {
 
     }
 
+    public void actualizarPedidos(){
+        Pedidos p = new Pedidos();
+        p.setUid(PedidosSelected.getUid());
+        p.setNumerPedido(numeroPedido.getText().toString().trim());
+        p.setNombre(clienteP.getText().toString().trim());
+        p.setCedula(cedulaP.getText().toString().trim());
+        p.setMenu(menuP.getText().toString().trim());
+        p.setMesa(mesaP.getText().toString().trim());
+        p.setExtras(extrasP.getText().toString().trim());
+        databaseReference.child("Pedidos").child(p.getUid()).setValue(p);
+        limpiarCajas();
+    }
+    public void eliminarPedidos(){
+        Personas p = new Personas();
+        p.setUid(PedidosSelected.getUid());
+        databaseReference.child("Pedidos").child(p.getUid()).removeValue();
+        limpiarCajas();
+    }
+
     private void inicializarFirebase(){
         FirebaseApp.initializeApp(this);
         firebaseDatabase = FirebaseDatabase.getInstance();
@@ -130,5 +186,27 @@ public class ingresarPedidos extends AppCompatActivity {
         }else if (pwd.equals("")){
             extrasP.setError("Required");
         }
+    }
+
+    private void listarPedidos() {
+        databaseReference.child("Pedidos").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listPedidos.clear();
+                for(DataSnapshot objectSnapshot1: dataSnapshot.getChildren()){
+                    Pedidos p = objectSnapshot1.getValue(Pedidos.class);
+                    listPedidos.add(p);
+
+                    arrayAdapterPedidos = new ArrayAdapter<Pedidos>(ingresarPedidos.this, android.R.layout.simple_list_item_1, listPedidos);
+                    lvPedidos2.setAdapter(arrayAdapterPedidos);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
